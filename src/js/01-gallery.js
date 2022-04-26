be38719cb2c288b812894b0ca480a107c3ebb21f
dimaparfenyk <92674961+dimaparfenyk@users.noreply.github.com>
2022-04-26 21:11:36 +0300

    update 1task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,37 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const galleryImageContainer = document.querySelector('.gallery')
+const galleryImageContainer = document.querySelector('.gallery');
 
 function createListGalleryItems(items) {
 
-  return items.map(item =>
-    `<div class="gallery__item">
-        <a class="gallery__link" 
+    return items.map(item => 
+        `<div class="gallery__item">
+        <a class="gallery__link"
+        data-lightbox="images" 
         href="${item.original}">
         <img class="gallery__image"
         src="${item.preview}" 
         data-source="${item.original}" 
         alt="${item.description}"/>
         </a>
-    </div>`).join("");
+    </div>`).join("")
 }
-
-console.log(createListGalleryItems(galleryItems))
 galleryImageContainer.innerHTML = createListGalleryItems(galleryItems);
 
-// galleryImageContainer.addEventListener('click', onGalleryImageContainerClick);
+galleryImageContainer.addEventListener('click', onGalleryImageContainerClick);
 
-// function onGalleryImageContainerClick(evt) {
-//   if (!evt.target.classList.contains('.gallery__image')) {
-//     return
-//   }
-// };
-console.log(galleryItems);
-// const instance = basicLightbox.create(` <img src="${modalImgToShow}" width="800" height="600">`,
-// {        closable: true,
-//  onShow: (instance) => { window.addEventListener('keydown', onModalPressEsc) },
-//  onClose: (instance) => {window.removeEventListener('keydown', onModalPressEsc)},
-//  });
+function onGalleryImageContainerClick(evt) {
+    
+    evt.preventDefault();
+    
+  if (!evt.target.classList.contains('gallery__image')) {
+    return 
+  }
+   
+    const selectedImg = evt.target;
+    const selectedImgLink = evt.target.closest('.gallery__link')
+    
+    selectedImg.src = selectedImgLink.href; 
+};
+
+galleryImageContainer.onclick = () => {
+
+	basicLightbox.create(`
+		<img width="1400" height="900" src="${onGalleryImageContainerClick}">
+	`).show()
+
+}
+
 
 
 
