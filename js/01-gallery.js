76ec14be09bdc96e19bcf6b07b6cb4931c8f2b78
Dmitrij <dimaparfenyk1990@gmail.com>
2023-02-03 00:28:51 +0200

    refactored code at gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,35 +1,72 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-const galleryImageContainer = document.querySelector('.gallery');
 
-function createListGalleryItems(items) {
+// const galleryImageContainer = document.querySelector('.gallery');
 
-    return items.map(item => 
-        `<div class="gallery__item">
-        <a class="gallery__link"
-        data-lightbox="images" 
-        href="${item.original}">
-        <img class="gallery__image"
-        src="${item.preview}" 
-        data-source="${item.original}" 
-        alt="${item.description}"/>
-        </a>
-    </div>`).join("")
-}
-galleryImageContainer.innerHTML = createListGalleryItems(galleryItems);
+// function createListGalleryItems(items) {
 
-galleryImageContainer.onclick = (evt) => {
+//     return items.map(item =>
+//         `<div class="gallery__item">
+//         <a class="gallery__link"
+//         data-lightbox="images"
+//         href="${item.original}">
+//         <img class="gallery__image"
+//         src="${item.preview}"
+//         data-source="${item.original}"
+//         alt="${item.description}"/>
+//         </a>
+//     </div>`).join("")
+// }
+// galleryImageContainer.innerHTML = createListGalleryItems(galleryItems);
+
+// galleryImageContainer.onclick = (evt) => {
   
-  evt.preventDefault();
+//   evt.preventDefault();
 
-   if (!evt.target.classList.contains('gallery__image')) {
-    return
-  }
+//    if (!evt.target.classList.contains('gallery__image')) {
+//     return
+//   }
   
-	basicLightbox.create(`
-		<img width="1400" height="900" src="${evt.target.dataset.source}">
-	`).show()
-}
+// 	basicLightbox.create(`
+// 		<img width="1400" height="900" src="${evt.target.dataset.source}">
+// 	`).show()
+// }
+
+
+const galleryList = document.querySelector(".gallery");
+
+const createGalleryMarkup = items => items
+  .map(({preview,original,description}) => `
+  <div class="gallery__item">
+  <a class="gallery__link" data-lightbox="images" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+  </div>`).join("");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const onSliderHandler = (e) => {
+  const src = e.target.dataset.source;
+  
+  if (!src) {
+    return;
+  };
+  const slider = basicLightbox.create(`
+    <img src="${src}" width="1400" height="900">
+`);
+    
+  slider.show();
+};
+
+galleryList.addEventListener('click', onSliderHandler);
+
+
 
 
 
